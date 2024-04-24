401bd10c6d1e5bab267480c29d348b724502a55f
jmolina-2022408 <jmolina-2022408@kinal.edu.gt>
2024-04-23 22:34:20 -0600

    Se agrego modelo hotel

---
+++ hotel.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const hotelSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    direction: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        uppercase: true,
+        enum: ['SIN CALIFICACIÓN', '1 ESTRELLA', '2 ESTRELLAS', '3 ESTRELLAS', '4 ESTRELLAS', '5 ESTRELLAS'],
+        default: 'SIN CALIFICACIÓN',
+        required: true
+    },
+    amenities: [{
+        type: String,
+        required: true
+    }]
+}, {
+    versionKey: false
+})
+
+export default mongoose.model('hotel', hotelSchema)
