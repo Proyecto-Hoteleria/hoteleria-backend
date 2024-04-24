316940258baa805cd9f174cd095a9d8f329266ec
jmolina-2022408 <jmolina-2022408@kinal.edu.gt>
2024-04-23 22:17:49 -0600

    Se agrego modelo usuario

---
+++ user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        minLength: [8, 'The password must be 8 characters long.'],
+        required: true
+    },
+    phone: {
+        type: String,
+        minLength: [8, 'The phone must be 8 characters long.'],
+        maxLength: [8, 'The password must be less than 8 characters.'],
+        required: true
+    },
+    role: {
+        type: String,
+        uppercase: true,
+        enum: ['ADMIN', 'CLIENT'],
+        required: true
+    }
+}, {
+    versionKey: false
+})
+
+export default mongoose.model('user', userSchema)
