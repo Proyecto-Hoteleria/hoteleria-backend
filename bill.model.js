4f61d0347c8e6f9a8d4a2a03d54571a1f8bb8efa
hjuarez-2019128 <125756031+hjuarez-2019128@users.noreply.github.com>
2024-04-26 19:17:42 -0600

    Se agrego el modelo de facturas

---
+++ bill.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const billSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    room: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'room',
+        required: true
+    },
+    hotel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'hotel',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    }
+}, {
+    versionKey: false
+});
+
+const Bill = mongoose.model('bill', billSchema);
+
+export default Bill;
